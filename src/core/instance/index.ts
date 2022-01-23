453fe89ff47ba6b3c442d13e1c76a31b5464d3e4
lndss <18352521335@163.com>
2022-01-23 22:15:58 +0800

    .

---
+++ src/core/instance/index.ts
@@ -0,0 +1,32 @@
+import { process } from "../../env";
+
+import { initMixin } from "./init";
+import { stateMixin } from "./state";
+import { renderMixin } from "./render";
+import { eventsMixin } from "./events";
+import { lifecycleMixin } from "./lifecycle";
+import { warn } from "../util/index";
+
+/**
+ * @constructor Vue
+ * @param {*} options
+ */
+class Vue {
+  private _init: (options: any) => void;
+
+  constructor(options: any) {
+    if (process.env.NODE_ENV !== "production" && !(this instanceof Vue)) {
+      warn("Vue is a constructor and should be called with the `new` keyword");
+    }
+
+    this._init(options);
+  }
+}
+
+initMixin(Vue);
+stateMixin(Vue);
+eventsMixin(Vue);
+lifecycleMixin(Vue);
+renderMixin(Vue);
+
+export default Vue;
