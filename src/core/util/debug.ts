acf10867aa880e5126c18b69f9a92ce33d5ae150
lndss <18352521335@163.com>
2022-01-24 23:05:45 +0800

    Revert "build: release 2.6.14"
    This reverts commit 612fb89547711cacb030a3893a0065b785802860.

--- src/core/util/debug.ts
+++
@@ -1,7 +0,0 @@
-import config from "../config";
-import { noop } from "../../shared/util";
-
-export let warn = noop;
-export let tip = noop;
-export let generateComponentTrace = noop;
-export let formatComponentName = noop;
--- src/core/vdom/vnode.ts
+++
@@ -1,3 +0,0 @@
-export default class VNode {
-  tag?: string;
-}
